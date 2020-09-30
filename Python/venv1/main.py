8abd03fcbf9633b26c911c6f448331e7f1511f24
wkopacz6 <43361034+wkopacz6@users.noreply.github.com>
2020-09-30 14:09:08 -0400

    Tried something with old demo

--- Python/venv1/main.py
+++ Python/venv1/main.py
@@ -8,6 +8,7 @@ import librosa
 from scipy.io.wavfile import read
 import mido
 
+
 filename = '/Users/walterkopacz/PycharmProjects/BeatVoxPrototype/venv1/BeatVoxTest1.wav'
 hoplen = 512
 nfft = 2048
@@ -16,11 +17,74 @@ nfft = 2048
 y1, fs = librosa.load(filename, sr=44100)
 
 #Onset detection
-onsets_envelope = librosa.onset.onset_strength(y=y1, sr=fs, hop_length=hoplen)
-onsets = librosa.onset.onset_detect(y=y1, sr=fs, hop_length=hoplen, units='frames', onset_envelope=onsets_envelope, backtrack=True)
-onsets_time = librosa.frames_to_time(onsets, sr=fs)
-print(onsets)
+def compute_novelty_function(x, method, end=None):
+    #normalize audio
+    x = x/max(abs(x))
+    # use librosa to calculate the stft
+    X = librosa.core.stft(x, n_fft=1024, hop_length=768)
+    if method == 'log-mag':
 
+        #compute the log-magnitude spectrum
+        log_mag_X = 20*np.log10(np.abs(X))
+        log_mag_X = np.c_[log_mag_X[:, 0], log_mag_X]
+
+        diff_log_mag_X = np.diff(log_mag_X, 1, axis=1)
+
+        # RMS of diff
+        flux = (diff_log_mag_X).sum(axis=0) / log_mag_X.shape[0]
+
+        return (flux)
+    #Directly from pyACA
+    elif method == 'hains':
+        epsilon = 1e-5
+
+        # difference spectrum (set first diff to zero)
+        X = np.c_[X[:, 0], np.sqrt(X)]
+        X[X <= 0] = epsilon
+
+        afDiff = np.log2(X[:, np.arange(1, X.shape[1])] / X[:, np.arange(0, X.shape[1] - 1)])
+
+        # flux
+        d_hai = np.sum(afDiff, axis=0) / X.shape[0]
+
+        return (d_hai)
+    #Directly from pyACA
+    elif method == 'laroche':
+        # difference spectrum (set first diff to zero)
+        X = np.c_[X[:, 0], np.sqrt(X)]
+
+        afDiff = np.diff(X, 1, axis=1)
+
+        # flux
+        d_lar = np.sum(afDiff, axis=0) / X.shape[0]
+
+        return (d_lar)
+
+    end
+
+#moving average filter used in peak picking
+def moving_average(a, n) :
+    a = np.concatenate((np.zeros(n-1), a))
+    ret = np.cumsum(a, dtype=float)
+    ret[n:] = ret[n:] - ret[:-n]
+    return ret[n - 1:] / n
+
+def pick_peaks(nov, thres):
+    #determine adaptive threshold
+    #take the moving average of the novelty function
+    nov = moving_average(nov, n=2)
+
+    #use scipy find peaks
+    peaks = sp.signal.find_peaks(nov, height=thres)
+    # convert peak blocks to peak times
+    onsets = librosa.core.frames_to_time(peaks[0], n_fft=1024, hop_length=768, sr=44100)
+    return (peaks[0], onsets)
+
+    end
+
+
+nov = compute_novelty_function(y1, 'log-mag', end=None)
+onsets, onsets_time = pick_peaks(nov, .28)
 
 
 #Timbre Detection
