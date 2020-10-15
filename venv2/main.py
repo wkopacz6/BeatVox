6bfb2f2941e60074a0c599dd98ccae95ae4febb4
joshuachan883 <71687102+joshuachan883@users.noreply.github.com>
2020-10-15 17:59:18 +0800

    fixes
    corrected the normalization, added cross validation

--- venv2/main.py
+++ venv2/main.py
@@ -2,31 +2,40 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import os
 
+from scipy.sparse import csr_matrix
 import librosa as librosa
 import numpy as np
 import pandas as pd
+from libsvm.commonutil import csr_find_scale_param, csr_scale
 from libsvm.svm import svm_problem, svm_parameter
 from libsvm.svmutil import *
+import os
 
 # normalize audio file function
 def normalize(audio_sample):
     return audio_sample / max(abs(audio_sample))
 
-def min_max_normalization(x):
-    x = np.asarray(x)
-    min_val = np.amin(np.absolute(x))
-    max_val = np.amax(np.absolute(x))
-    x_scaled = (x - min_val)/(max_val-min_val)
-    return x_scaled
+
+def normalize_training(train):
+    train = np.asarray(train)
+    min_val = np.amin(train, axis=0)
+    max_val = np.amax(train, axis=0)
+    train_scaled = (train - min_val) / (max_val - min_val)
+    return train_scaled, min_val, max_val
+
+
+def normalize_testing(test, min_vec, max_vec):
+    test_scaled = (test - min_vec) / (max_vec - min_vec)
+    return test_scaled
+
 
 def test_classification(audio_path, ground_truth_path):
     # this code counts the total number of beats in a given folder of onset files
     os.chdir(ground_truth_path)
     csv_name = os.listdir()
     total_beat_count = 0
-    starting_index = np.array([])
+
 
     for onset_file in csv_name:
         current_csv = pd.read_csv(onset_file)
@@ -43,9 +52,8 @@ def test_classification(audio_path, ground_truth_path):
     # the beat index from count 0 across multiple audio files in beatboxset1
     index = 0
 
-
     for audio in file_name:
-        starting_index = np.append(starting_index, np.array(int(index)))
+        print('start parsing ' + audio)
         y, sr = librosa.core.load(audio)
         y = normalize(y)
         os.chdir(ground_truth_path)
@@ -59,7 +67,8 @@ def test_classification(audio_path, ground_truth_path):
                 label[index] = 2
             # elif current_onset['Type'][x] == 'ho':
             #     label[index] = 3
-            elif current_onset['Type'][x] == 'sb' or current_onset['Type'][x] == 'sk' or current_onset['Type'][x] == 's':
+            elif current_onset['Type'][x] == 'sb' or current_onset['Type'][x] == 'sk' or current_onset['Type'][
+                x] == 's':
                 label[index] = 4
             # elif current_onset['Type'][x] == 'sk':
             #     label[index] = 5
@@ -76,12 +85,12 @@ def test_classification(audio_path, ground_truth_path):
             else:
                 label[index] = 11
 
-            print(str(current_onset['Timestamp'][x]) + ' ' + audio + ' ' + str(x))
+            print(str(current_onset['Timestamp'][x]) + ' ' + audio + ' ' + current_onset['Type'][x] + ' ' + str(x) + ' ' + str(index))
             start_sample = int(sr * current_onset['Timestamp'][x])
 
-            # if this beat is the last beat of the track
+            # if this beat is the last beat of the track, set it to 0.25 seconds
             if x == row - 1:
-                end_sample = len(y) - 1
+                end_sample = start_sample + int(sr * 0.25)
             else:
                 end_sample = int(sr * current_onset['Timestamp'][x + 1])
             single_beat = y[start_sample: end_sample: 1]
@@ -98,32 +107,40 @@ def test_classification(audio_path, ground_truth_path):
 
             os.chdir(audio_path)
 
-    # for i in range(len(starting_index)):
-    #     test_features = features[starting_index[i], starting_index[i+1]]
-    #     train_features = features[0:starting_index-1]
-    #features = min_max_normalization(features)
+
     print(index)
     print(features.shape)
     print(label)
-    #print(starting_index)
-
-    c_val = np.array([2**-5, 2**-3, 2**-1, 2**1, 2**3, 2**5, 2**7, 2**9, 2**11, 2**13, 2**15])
-    gamma = np.array([2**-15, 2**-13, 2**-11, 2**-9, 2**-7, 2**-5, 2**-3, 2**-1, 2**1, 2**3])
-    acc = np.empty([len(c_val), len(gamma)])
-    for p in range(len(c_val)):
-        for k in range(len(gamma)):
-            prob = svm_problem(label[1:-400:1], min_max_normalization(features[1:-400:1]))
-            param = svm_parameter('-t 0 -c ' + str(c_val[p]) + ' ' + '-g ' + str(gamma[k]))
-            model = svm_train(prob, param)
-            p_label, p_acc, p_val = svm_predict(label[-400:], min_max_normalization(features[-400:]), model)
-            acc[p,k] = p_acc[0]
-    print(acc)
+    # train_scaled, vec1, vec2 = normalize_training(features[1:-400:1])
+    # test_scaled = normalize_testing(features[-400:], vec1, vec2)
+    # pd.DataFrame(data=train_scaled).to_csv(r'C:\Users\Joshua\Desktop\MUSI 3770\train.csv', index = False, header = False)
+    # pd.DataFrame(data=label).to_csv(r'C:\Users\Joshua\Desktop\MUSI 3770\label.csv', index = False, header = False)
+    # pd.DataFrame(data=test_scaled).to_csv(r'C:\Users\Joshua\Desktop\MUSI 3770\test.csv', index=False, header=
+    acc = np.empty(10)
+    for r in range(10):
+        train_scaled, vec1, vec2 = normalize_training(np.concatenate((features[0:384*r:1], features[384*r+384::1])))
+        prob = svm_problem(np.concatenate((label[0:384*r:1], label[384*r+384::1])), train_scaled)
+        param = svm_parameter('-t 0 -c 100')
+        model = svm_train(prob, param)
+        p_label, p_acc, p_val = svm_predict(label[384*r:384*r + 384:1], normalize_testing(features[384*r:384*r + 384:1], vec1, vec2), model)
+        acc[r] = p_acc[0]
+    # still not to the point of testing c_val and gamma
+    # c_val = np.array([2**-5, 2**-3, 2**-1, 2**1, 2**3, 2**5, 2**7, 2**9, 2**11, 2**13, 2**15])
+    # gamma = np.array([2**-15, 2**-13, 2**-11, 2**-9, 2**-7, 2**-5, 2**-3, 2**-1, 2**1, 2**3])
+    # acc = np.empty([len(c_val), len(gamma)])
+    # for p in range(len(c_val)):
+    #     for k in range(len(gamma)):
+    #         prob = svm_problem(label[1:-400:1], min_max_normalization(features[1:-400:1]))
+    #         param = svm_parameter('-t 0 -c ' + str(c_val[p]) + ' ' + '-g ' + str(gamma[k]))
+    #         model = svm_train(prob, param)
+    #         p_label, p_acc, p_val = svm_predict(label[-400:], min_max_normalization(features[-400:]), model)
+    #         acc[p,k] = p_acc[0]
+    # print(acc)
     print('FINISHED')
-
-
 
 
 audio_path_test = r'C:\Users\Joshua\Documents\GitHub\3770python\beatboxset1'
 ground_truth_path_test = r'C:\Users\Joshua\Documents\GitHub\3770python\annotation'
 test_classification(audio_path_test, ground_truth_path_test)
+
 
