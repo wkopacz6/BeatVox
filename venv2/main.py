9348e3c8643539697948f888de9510ebd7386cb9
joshuachan883 <71687102+joshuachan883@users.noreply.github.com>
2020-10-22 20:58:30 +0800

    updated for midterm presentation

--- venv2/main.py
+++ venv2/main.py
@@ -2,6 +2,7 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from pyexpat import features
 
 from scipy.sparse import csr_matrix
 import librosa as librosa
@@ -16,6 +17,8 @@ import os
 def normalize(audio_sample):
     return audio_sample / max(abs(audio_sample))
 
+def extract_norm_param(train):
+    return
 
 def normalize_training(train):
     train = np.asarray(train)
@@ -35,7 +38,7 @@ def test_classification(audio_path, ground_truth_path):
     os.chdir(ground_truth_path)
     csv_name = os.listdir()
     total_beat_count = 0
-
+    #start_label = np.empty(14)
 
     for onset_file in csv_name:
         current_csv = pd.read_csv(onset_file)
@@ -43,7 +46,7 @@ def test_classification(audio_path, ground_truth_path):
         total_beat_count = total_beat_count + row
 
     # initialize empty feature vector (size = total number of beats * mfcc dimension)
-    features = np.empty([total_beat_count, 15])
+    features = np.empty([total_beat_count, 18])
     label = np.empty([total_beat_count])
 
     os.chdir(audio_path)
@@ -62,14 +65,14 @@ def test_classification(audio_path, ground_truth_path):
 
         for x in range(row):
             if current_onset['Type'][x] == 'k':
-                label[index] = 1
+                label[index] = 36
             elif current_onset['Type'][x] == 'hc' or current_onset['Type'][x] == 'ho':
-                label[index] = 2
+                label[index] = 38
             # elif current_onset['Type'][x] == 'ho':
             #     label[index] = 3
             elif current_onset['Type'][x] == 'sb' or current_onset['Type'][x] == 'sk' or current_onset['Type'][
                 x] == 's':
-                label[index] = 4
+                label[index] = 42
             # elif current_onset['Type'][x] == 'sk':
             #     label[index] = 5
             # elif current_onset['Type'][x] == 's':
@@ -92,7 +95,8 @@ def test_classification(audio_path, ground_truth_path):
             if x == row - 1:
                 end_sample = start_sample + int(sr * 0.25)
             else:
-                end_sample = int(sr * current_onset['Timestamp'][x + 1])
+                end_sample = int(start_sample + 0.04*sr)
+                #end_sample = int((sr * current_onset['Timestamp'][x + 1] - start_sample)*0.2) + start_sample
             single_beat = y[start_sample: end_sample: 1]
 
             # if fft window is larger than the total samples of the beat, pad zeros to both left and right sides of the signal
@@ -100,7 +104,7 @@ def test_classification(audio_path, ground_truth_path):
             if n_fft_window > single_beat.shape[-1]:
                 single_beat = np.pad(single_beat, int(n_fft_window // 2), mode='reflect')
 
-            mfccs = librosa.feature.mfcc(y=single_beat, n_mfcc=15, n_fft=n_fft_window)
+            mfccs = librosa.feature.mfcc(y=single_beat, n_mfcc=18, n_fft=n_fft_window)
             feature_vector = np.mean(mfccs, axis=1)
             features[index, :] = feature_vector
             index = index + 1
@@ -117,13 +121,20 @@ def test_classification(audio_path, ground_truth_path):
     # pd.DataFrame(data=label).to_csv(r'C:\Users\Joshua\Desktop\MUSI 3770\label.csv', index = False, header = False)
     # pd.DataFrame(data=test_scaled).to_csv(r'C:\Users\Joshua\Desktop\MUSI 3770\test.csv', index=False, header=
     acc = np.empty(10)
-    for r in range(10):
-        train_scaled, vec1, vec2 = normalize_training(np.concatenate((features[0:384*r:1], features[384*r+384::1])))
-        prob = svm_problem(np.concatenate((label[0:384*r:1], label[384*r+384::1])), train_scaled)
-        param = svm_parameter('-t 0 -c 100')
-        model = svm_train(prob, param)
-        p_label, p_acc, p_val = svm_predict(label[384*r:384*r + 384:1], normalize_testing(features[384*r:384*r + 384:1], vec1, vec2), model)
-        acc[r] = p_acc[0]
+    train_scaled, vec1, vec2 = normalize_training(features)
+    prob = svm_problem(label, train_scaled)
+    param = svm_parameter('-t 0 -c 100')
+    model = svm_train(prob, param)
+    os.chdir(r'C:\Users\Joshua\Documents\GitHub\3770python')
+    svm_save_model('model_file', model)
+
+    # for r in range(10):
+    #     train_scaled, vec1, vec2 = normalize_training(np.concatenate((features[0:384*r:1], features[384*r+384::1])))
+    #     prob = svm_problem(np.concatenate((label[0:384*r:1], label[384*r+384::1])), train_scaled)
+    #     param = svm_parameter('-t 0 -c 100')
+    #     model = svm_train(prob, param)
+    #     p_label, p_acc, p_val = svm_predict(label[384*r:384*r + 384:1], normalize_testing(features[384*r:384*r + 384:1], vec1, vec2), model)
+    #     acc[r] = p_acc[0]
     # still not to the point of testing c_val and gamma
     # c_val = np.array([2**-5, 2**-3, 2**-1, 2**1, 2**3, 2**5, 2**7, 2**9, 2**11, 2**13, 2**15])
     # gamma = np.array([2**-15, 2**-13, 2**-11, 2**-9, 2**-7, 2**-5, 2**-3, 2**-1, 2**1, 2**3])
@@ -136,11 +147,29 @@ def test_classification(audio_path, ground_truth_path):
     #         p_label, p_acc, p_val = svm_predict(label[-400:], min_max_normalization(features[-400:]), model)
     #         acc[p,k] = p_acc[0]
     # print(acc)
+    print(np.mean(acc))
     print('FINISHED')
 
 
-audio_path_test = 'r'C:\Users\Joshua\Documents\GitHub\3770python\beatboxset1''
+audio_path_test = r'C:\Users\Joshua\Documents\GitHub\3770python\beatboxset1'
 ground_truth_path_test = r'C:\Users\Joshua\Documents\GitHub\3770python\annotation'
 test_classification(audio_path_test, ground_truth_path_test)
 
 
+vec2 = np.array([99.39611816, 218.22731018, 83.60921478, 102.21869659,  67.80625153,  67.14355469,  40.61268234,  54.90848923,  29.11833954,  43.41244125,  27.29264832,  35.02856827,  30.24285698, 30.17704964, 33.65706635,  24.62182045, 24.663414, 26.44494438])
+vec1 = np.array([-519.73138428, -61.01974869,-120.05568695, -53.44915771, -65.82631683,  -44.67581177,  -70.60654449,  -40.54338074,  -37.27917099, -25.75437927,  -28.90529633, -31.82174873, -29.48809814, -29.12382698, -29.27874565,  -26.87375069, -29.37361908, -26.73584938])
+fs = 44100
+n_fft_window = 2048
+param = svm_parameter('-t 0 -c 100')
+model = svm_load_model('model_file')
+def demo(segment):
+    if len(segment) < 0.04 * fs:
+        np.pad(segment, int(0.04 * fs // 2), mode='reflect')
+    else:
+        segment = segment[0:int(0.04 * fs)]
+    if n_fft_window > segment.shape[-1]:
+        segment = np.pad(segment, int(n_fft_window // 2), mode='reflect')
+    single_vec = librosa.feature.mfcc(y=segment, n_mfcc=18, n_fft=n_fft_window)
+    feature_vector = np.mean(single_vec, axis=1)
+    p_label, p_acc, p_val = svm_predict([], normalize_testing(feature_vector, vec1, vec2), model)
+    return p_label
