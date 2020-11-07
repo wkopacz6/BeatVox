5138735e49624b3f02b2321c3f7ca16f1048d9c6
JohnKasun <johnkasun@live.com>
2020-11-07 08:34:44 -0500

    Added classification files

--- JUCE/BeatVOX_main/Source/recordAudio.cpp
+++ JUCE/BeatVOX_main/Source/recordAudio.cpp
@@ -40,7 +40,9 @@ recordAudio::recordAudio()
 
 recordAudio::~recordAudio()
 {
+    bufferRecordedAudio.~AudioBuffer();
     bufferRecordedAudio.setSize(0,0);
+    
     bufferMidi.clear();
 
     shutdownAudio();
